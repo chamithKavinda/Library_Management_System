99c4087074483a1f54f3d874e6e88050b3a07bd5
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-05 17:31:07 +0530

    designed LoginDashboard fxml

---
+++ src/main/java/lk/ijse/controller/LoginDashboard.java
@@ -0,0 +1,19 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoginDashboard {
+
+    @FXML
+    void adminbtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userbtnOnAction(ActionEvent event) {
+
+    }
+
+}
+
