38b80442b6e7d356afc2f601cfbdd7e1fbe8750f
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-10 20:23:54 +0530

    implement UserDAOImpl

---
+++ src/main/java/lk/ijse/dao/custom/UserDAO.java
@@ -0,0 +1,16 @@
+package lk.ijse.dao.custom;
+
+import lk.ijse.dao.CrudDAO;
+import lk.ijse.entity.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface UserDAO extends CrudDAO<User> {
+
+    List<User> getAll();
+
+    boolean save(User entity);
+
+    boolean exist(String UserName, String UserPassword) throws SQLException, ClassNotFoundException;
+}
--- src/main/java/lk/ijse/dao/custom/impl/UserDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/UserDAOImpl.java
@@ -1,4 +1,57 @@
 package lk.ijse.dao.custom.impl;
 
-public class UserDAOImpl {
+import lk.ijse.config.SessionFactoryConfig;
+import lk.ijse.dao.custom.UserDAO;
+import lk.ijse.entity.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+    @Override
+    public List<User> getAll(){
+        Session session = SessionFactoryConfig.getInstance().getSession();
+        String sql = "FROM User";
+        Query query = session.createQuery(sql);
+        List<User> userList = query.list();
+        session.close();
+        return userList;
+    }
+
+    @Override
+    public boolean save(User entity){
+        Session session = SessionFactoryConfig.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.save(entity);
+            transaction.commit();
+            return true;
+        }catch (Exception e){
+            transaction.rollback();
+            return false;
+        }finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public boolean exist(String UserName, String UserPassword) throws SQLException, ClassNotFoundException {
+        Session session = SessionFactoryConfig.getInstance().getSession();
+        try  {
+            String hql = "SELECT COUNT(*) FROM User WHERE user_name = :username AND user_password = :password";
+            Query<Long> query = session.createQuery(hql, Long.class);
+            query.setParameter("username", UserName);
+            query.setParameter("password", UserPassword);
+
+            Long count = query.uniqueResult();
+            return count != null && count > 0;
+        } catch (Exception e) {
+            // Handle exceptions
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
