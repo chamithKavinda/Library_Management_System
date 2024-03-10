06fccc0acae379091fdbf9838d1c3a5d99886b7e
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-10 18:44:17 +0530

    User entity done

--- src/main/java/lk/ijse/controller/UserSignInFormController.java
+++ src/main/java/lk/ijse/controller/UserSignInFormController.java
@@ -1,4 +1,25 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class UserSignInFormController {
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    private TextField txtUserPassword;
+
+    @FXML
+    void btnUserSignInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hyperUserSignUpOnAction(ActionEvent event) {
+
+    }
+
 }
---
+++ src/main/java/lk/ijse/entity/User.java
@@ -0,0 +1,27 @@
+package lk.ijse.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Id
+    @Column(name = "user_email" , length = 30)
+    private String email;
+
+    @Column(name = "admin_password")
+    private String password;
+}
