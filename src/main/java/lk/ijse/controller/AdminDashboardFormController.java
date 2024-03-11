2c2b7bed115ce05f7dbd897a8cec0f3c4029e4d8
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-11 08:02:25 +0530

    AdminDashboard logout feature implement

--- src/main/java/lk/ijse/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/controller/AdminDashboardFormController.java
@@ -1,4 +1,65 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class AdminDashboardFormController {
+
+    @FXML
+    private AnchorPane AdminDashbordForm;
+
+    @FXML
+    private Label date;
+
+    @FXML
+    private Label time;
+
+    @FXML
+    void btnBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBranchesOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/admin/AdminSignInPage.fxml"));
+        Scene scene = new Scene(rootNode);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("Login");
+        stage.show();
+
+        //Close the Current Window
+        Stage dashboard= (Stage) AdminDashbordForm.getScene().getWindow();
+        dashboard.close();
+    }
+
+    @FXML
+    void btnRecordsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUsersOnAction(ActionEvent event) {
+
+    }
+
 }
