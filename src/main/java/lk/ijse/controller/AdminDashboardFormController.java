03b093618a4e231c4cb660c66495683ecb51a518
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-12 15:35:19 +0530

    AdminDashboard date , time added

--- src/main/java/lk/ijse/controller/AdminDashboardFormController.java
+++ src/main/java/lk/ijse/controller/AdminDashboardFormController.java
@@ -1,15 +1,19 @@
 package lk.ijse.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AdminDashboardFormController {
 
@@ -21,9 +25,16 @@ public class AdminDashboardFormController {
 
     @FXML
     private Label date;
+    private volatile boolean stop  = false;
 
     @FXML
     private Label time;
+    private volatile boolean s  = false;
+
+    public void initialize() {
+        TimeNow();
+        Date();
+    }
 
     @FXML
     void btnBooksOnAction(ActionEvent event) throws IOException {
@@ -76,4 +87,39 @@ public class AdminDashboardFormController {
         this.root.getChildren().add(anchorPane);
     }
 
+    private void TimeNow(){
+        Thread thread = new Thread(()->{
+            SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+            while(!stop){
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){
+                    new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+                }
+                final String timeNow = sdf.format(new Date());
+                Platform.runLater(()->{
+                    time.setText(timeNow);
+                });
+            }
+        });
+        thread.start();
+    }
+
+    private void Date(){
+        Thread thread = new Thread(()->{
+            SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+            while (!s){
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){
+                    new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+                }
+                final String dateNow = sdf1.format(new Date());
+                Platform.runLater(()->{
+                    date.setText(dateNow);
+                });
+            }
+        });
+        thread.start();
+    }
 }
