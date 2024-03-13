16a0b63db5b9d5ad2a47d108bd1b5897c7f1987d
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-13 16:04:28 +0530

    added UserTm

--- src/main/java/lk/ijse/controller/admin/AdminUsersFormController.java
+++ src/main/java/lk/ijse/controller/admin/AdminUsersFormController.java
@@ -1,4 +1,58 @@
 package lk.ijse.controller.admin;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class AdminUsersFormController {
+
+    @FXML
+    private TableColumn<?, ?> colUserEmail;
+
+    @FXML
+    private TableColumn<?, ?> colUserName;
+
+    @FXML
+    private TableColumn<?, ?> colUserPassword;
+
+    @FXML
+    private TableView<?> tblUser;
+
+    @FXML
+    private TextField txtUserEmail;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    private TextField txtUserPassword;
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+        clearFields();
+    }
+
+    private void clearFields(){
+        txtUserName.setText("");
+        txtUserEmail.setText("");
+        txtUserPassword.setText("");
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
---
+++ src/main/java/lk/ijse/dto/tm/UserTm.java
@@ -0,0 +1,14 @@
+package lk.ijse.dto.tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserTm {
+    private String name;
+    private String email;
+    private String password;
+}
