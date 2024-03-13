792186e852a43c2203973e87921bd6c70dd7a146
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-13 11:40:18 +0530

    borrow books history form designed

--- src/main/java/lk/ijse/controller/UserDashboardFormController.java
+++ src/main/java/lk/ijse/controller/UserDashboardFormController.java
@@ -1,20 +1,40 @@
 package lk.ijse.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class UserDashboardFormController {
 
+    @FXML
+    private Label date;
+    private volatile boolean stop  = false;
+
+    @FXML
+    private Label time;
+    private volatile boolean s  = false;
+
+    @FXML
+    private AnchorPane pane;
 
     @FXML
     private AnchorPane UserDashboard;
+
+    public void initialize() {
+        TimeNow();
+        Date();
+    }
 
     @FXML
     void btnBooksOnAction(ActionEvent event) {
@@ -22,13 +42,19 @@ public class UserDashboardFormController {
     }
 
     @FXML
-    void btnDashbordOnAction(ActionEvent event) {
-
+    void btnDashbordOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane;
+        anchorPane = FXMLLoader.<AnchorPane>load(getClass().getResource("/view/user/UserHomePage.fxml"));
+        this.pane.getChildren().clear();
+        this.pane.getChildren().add(anchorPane);
     }
 
     @FXML
-    void btnHistoryOnAction(ActionEvent event) {
-
+    void btnHistoryOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane;
+        anchorPane = FXMLLoader.<AnchorPane>load(getClass().getResource("/view/user/History.fxml"));
+        this.pane.getChildren().clear();
+        this.pane.getChildren().add(anchorPane);
     }
 
     @FXML
@@ -49,5 +75,43 @@ public class UserDashboardFormController {
     void btnSettingsOnAction(ActionEvent event) {
 
     }
+
+    private void TimeNow(){
+        Thread thread = new Thread(()->{
+            SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+            while(!stop){
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){
+                    new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+                }
+                final String timeNow = sdf.format(new Date());
+                Platform.runLater(()->{
+                    time.setText(timeNow);
+                });
+            }
+        });
+        thread.start();
+    }
+
+    private void Date(){
+        Thread thread = new Thread(()->{
+            SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+            while (!s){
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){
+                    new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+                }
+                final String dateNow = sdf1.format(new Date());
+                Platform.runLater(()->{
+                    date.setText(dateNow);
+                });
+            }
+        });
+        thread.start();
+    }
+
+
 
 }
