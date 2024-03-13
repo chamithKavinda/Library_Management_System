069b3b9a589147f53a8c1e3d590e8d23556c97c0
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-13 06:39:57 +0530

    admin Dashboard date , time added

--- src/main/java/lk/ijse/controller/AdminHomeFormController.java
+++ src/main/java/lk/ijse/controller/AdminHomeFormController.java
@@ -1,18 +1,66 @@
 package lk.ijse.controller;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AdminHomeFormController {
 
     @FXML
     private Label date;
+    private volatile boolean stop  = false;
 
     @FXML
     private AnchorPane root;
 
     @FXML
     private Label time;
+    private volatile boolean s  = false;
+
+    public void initialize() {
+        TimeNow();
+        Date();
+    }
+
+    private void TimeNow(){
+        Thread thread = new Thread(()->{
+            SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+            while(!stop){
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){
+                    new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+                }
+                final String timeNow = sdf.format(new Date());
+                Platform.runLater(()->{
+                    time.setText(timeNow);
+                });
+            }
+        });
+        thread.start();
+    }
+
+    private void Date(){
+        Thread thread = new Thread(()->{
+            SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+            while (!s){
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){
+                    new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+                }
+                final String dateNow = sdf1.format(new Date());
+                Platform.runLater(()->{
+                    date.setText(dateNow);
+                });
+            }
+        });
+        thread.start();
+    }
 
 }
