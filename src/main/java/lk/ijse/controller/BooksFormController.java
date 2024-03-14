4d00fdd4147f696cd1653870380622b434fa773b
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-14 12:06:12 +0530

    fxml updated

--- src/main/java/lk/ijse/controller/BooksFormController.java
+++ src/main/java/lk/ijse/controller/BooksFormController.java
@@ -1,8 +1,74 @@
 package lk.ijse.controller;
 
 
-public class BooksFormController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.bo.BOFactory;
+import lk.ijse.bo.custom.BooksBO;
+import lk.ijse.dto.BooksDto;
+import lk.ijse.dto.tm.BooksTm;
 
+import java.sql.SQLException;
+import java.util.List;
+
+public class BooksFormController {
+    @FXML
+    private TableColumn<?, ?> colAuthor;
+
+    @FXML
+    private TableColumn<?, ?> colBookId;
+
+    @FXML
+    private TableColumn<?, ?> colGenre;
+
+    @FXML
+    private TableColumn<?, ?> colStatus;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private TableView<BooksTm> tblBooks;
+
+    BooksBO booksBO = (BooksBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.BOOKS_BO);
+    public void initialize() {
+        setCellValueFactory();
+        loadAllBooks();
+    }
+
+    private void setCellValueFactory() {
+        colBookId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        colAuthor.setCellValueFactory(new PropertyValueFactory<>("author"));
+        colGenre.setCellValueFactory(new PropertyValueFactory<>("genre"));
+        colStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+    }
+
+    private void loadAllBooks(){
+        ObservableList<BooksTm> obList = FXCollections.observableArrayList();
+
+        try{
+            List<BooksDto> dtoList = booksBO.getAllBooks();
+
+            for (BooksDto dto: dtoList){
+                obList.add(
+                        new BooksTm(
+                                dto.getId(),
+                                dto.getTitle(),
+                                dto.getAuthor(),
+                                dto.getGenre(),
+                                dto.getStatus()));
+            }
+            tblBooks.setItems(obList);
+            tblBooks.refresh();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
 
 
 
