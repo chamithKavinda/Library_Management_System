d2124712e1f1305cf895d72927d54c8514931fdb
chamithKavinda <chamith13kavinda@gmail.com>
2024-03-15 12:54:58 +0530

    Implement Books Form

--- src/main/java/lk/ijse/controller/user/BooksFormController.java
+++ src/main/java/lk/ijse/controller/user/BooksFormController.java
@@ -4,13 +4,16 @@ package lk.ijse.controller.user;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import lk.ijse.bo.BOFactory;
 import lk.ijse.bo.custom.BooksBO;
+import lk.ijse.bo.custom.RecordsBO;
 import lk.ijse.dto.BooksDto;
+import lk.ijse.dto.RecordsDto;
 import lk.ijse.dto.tm.BooksTm;
 
 import java.awt.*;
@@ -40,8 +43,15 @@ public class BooksFormController {
     @FXML
     private TextField txtBookId;
 
+    @FXML
+    private DatePicker txtReturnDate;
+
+    @FXML
+    private TextField txtUserEmail;
 
     BooksBO booksBO = (BooksBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.BOOKS_BO);
+
+    RecordsBO recordsBO = (RecordsBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.RECORDS_BO);
     public void initialize() {
         setCellValueFactory();
         loadAllBooks();
@@ -77,8 +87,23 @@ public class BooksFormController {
         }
     }
 
+    public void btnSaveOnAction1(javafx.event.ActionEvent actionEvent) {
+        /*try{
+            RecordsDto dto = new RecordsDto();
 
+            dto.setId(txtBookId.getText());
+            dto.setEmail(txtUserEmail.getText());
+            dto.setReturnDate(txtReturnDate.getAccessibleText());
 
+            boolean isSaved = recordsBO.saveRecord(dto);
+            if (isSaved){
+                new Alert(Alert.AlertType.CONFIRMATION, "Book Borrowed Sucessfully!").show();
+            }else {
+                new Alert(Alert.AlertType.ERROR, "Book Borrowed failed!").show();
+            }
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }*/
 
-
+    }
 }
